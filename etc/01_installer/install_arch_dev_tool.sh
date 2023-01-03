6f103b0776906047c2a4b3f2a5bb245bbceeb614
marsh31 <marsh22h53@gmail.com>
2023-01-03 14:34:19 +0900

    add: dev tool installer

--- etc/01_installer/install_arch_dev_tool.sh
+++ etc/01_installer/install_arch_dev_tool.sh
@@ -9,29 +9,32 @@
 #
 
 
-# command_name;package_name(:package_name)*
+# <command_name>;<package_name>(:<package_name>)*;<handler:null or func_name>
+#
 pacman_install_items=(
-  "blender;blender"
-  "conky;conky"
-  "feh;feh"
-  "flameshot;flameshot"
-  "java;jre-openjdk:jdk-openjdk"
-  "jq;jq"
-  "mpv;mpv"
-  "ranger;ranger"
-  "rg;ripgrep"
-  "rofi;rofi"
-  "tig;tig"
-  "tmux;tmux"
-  "unzip;unzip"
-  "vlc;vlc"
-  "xsel;xsel"
+  "blender;blender;"
+  "conky;conky;"
+  "docker;docker;handler_docker"
+  "docker-compose;docker-compose;"
+  "feh;feh;"
+  "flameshot;flameshot;"
+  "java;jre-openjdk:jdk-openjdk;"
+  "jq;jq;"
+  "mpv;mpv;"
+  "ranger;ranger;"
+  "rg;ripgrep;"
+  "rofi;rofi;"
+  "tig;tig;"
+  "tmux;tmux;"
+  "unzip;unzip;"
+  "vlc;vlc;"
+  "xsel;xsel;"
 )
 
 
 yay_install_items=(
-  "android-studio;android-studio"
-  "ghq;ghq"
+  "android-studio;android-studio;"
+  "ghq;ghq;"
 )
 
 
@@ -48,13 +51,20 @@ is_exist_command() {
 }
 
 
+handler_docker() {
+  echo "handler_docker"
+#  sudo systemctl start docker
+#  sudo systemctl enable docker
+}
+
+
 install_items() {
   local arr
   local item
   local sub_str=""
   local tmp
-  local command_name
-  local package_name
+  local command_name package_name handler
+
 
   arr=(${1})
   for item in "${arr[@]}"; do
@@ -73,31 +83,93 @@ install_items() {
 }
 
 
-do_install_pacman_items() {
-  local list
-  list=$(install_items "${pacman_install_items[*]}")
+handler_items() {
+  local arr
+  local item
+  local sub_str=""
+  local tmp
+  local command_name package_name handler_name
 
-  if [[ -n "$list" ]]; then
-    echo "sudo pacman -S $list"
-    sudo pacman -S $list
+
+  arr=(${1})
+  for item in "${arr[@]}"; do
+    tmp=(${item//;/ })
+    command_name="${tmp[0]}"
+    package_name="${tmp[1]}"
+    handler_name="${tmp[2]}"
+
+    # echo "$command_name, $package_name, ..."
+    is_exist_command $command_name
+    if [[ $? -eq 0 ]] && [[ -n "${handler_name}" ]]; then
+      sub_str="$sub_str ${handler_name//:/ }"
+    fi
+  done
+
+  echo "$sub_str"
+}
+
+
+do_install_pacman_items() {
+  local package_list handler_list
+  local func_arr
+
+  package_list=$(install_items "${pacman_install_items[*]}")
+  handler_list=$(handler_items "${pacman_install_items[*]}")
+
+  if [[ -n "$package_list" ]]; then
+    echo "sudo pacman -S $package_list"
+    #sudo pacman -S $list
   fi
+
+  func_arr=(${handler_list})
+  for func in "${func_arr[@]}"; do
+    ${func}
+  done
+
 }
 
 
 do_install_yay_items() {
-  local list
-  list=$(install_items "${yay_install_items[*]}")
+  local package_list handler_list
+  package_list=$(install_items "${yay_install_items[*]}")
+  handler_list=$(handler_items "${yay_install_items[*]}")
 
-  if [[ -n "$list" ]]; then
-    echo "yay -S $list"
-    yay -S $list
+  if [[ -n "$package_list" ]]; then
+    echo "yay -S $package_list"
+    #yay -S $list
   fi
+
+  func_arr=(${handler_list})
+  for func in "${func_arr[@]}"; do
+    ${func}
+  done
 }
 
 
+do_install() {
+  local cmd data_list
+  local package_list handler_list
+
+  cmd="${1}"
+  data_list="${2}"
+
+  package_list=$(install_items "${data_list[*]}")
+  handler_list=$(handler_items "${data_list[*]}")
+
+  if [[ -n "$package_list" ]]; then
+    echo "${cmd} ${package_list}"
+    ${cmd} ${package_list}
+  fi
+
+  func_arr=(${handler_list})
+  for func in "${func_arr[@]}"; do
+    ${func}
+  done
+}
+
 main() {
-  do_install_pacman_items
-  do_install_yay_items
+  do_install "sudo pacman -S" "${pacman_install_items[*]}"
+  do_install "yay -S" "${yay_install_items[*]}"
 }
 
 
