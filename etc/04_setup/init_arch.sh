a5ac6b8f7c2d05433a6fff939b5bd79c836804f9
marsh31 <marsh22h53@gmail.com>
2023-01-04 15:11:54 +0900

    add: setup scripts

---
+++ etc/04_setup/init_arch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# NAME:   init_arch.sh
+# AUTHOR: marsh
+# NOTE:
+#
+#   initial setup in arch linux.
+#
+
+./setup_keyrepeat.sh
+./setup_loadxmodmap.sh
+
+./setup_pacman_update.sh   # unused
+./setup_homeDirLocalJ2E.sh # unused
+
