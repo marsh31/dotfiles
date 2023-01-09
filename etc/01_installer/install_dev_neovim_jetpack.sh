b311ac80c1ea29ec9fbfdbc07af3f1d8f7ba8ac3
marsh31 <marsh22h53@gmail.com>
2023-01-09 18:09:42 +0900

    fix: setting file

--- etc/01_installer/install_dev_neovim_jetpack.sh
+++ etc/01_installer/install_dev_neovim_jetpack.sh
@@ -6,6 +6,6 @@
 # NOTE:
 # 	install nerd fornts.
 
-curl -fLo ~/.config/nvim/autoload/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
+# curl -fLo ~/.config/nvim/autoload/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
 
 # curl -fLo ~/.local/share/nvim/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
