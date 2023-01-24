a0c1f7ebcc6a4c29c38c223c1b714fb76f4ba191
marsh31 <marsh22h53@gmail.com>
2023-01-24 19:53:23 +0900

    add: snippets

--- config/nvim/lua/snippets/init.lua
+++ config/nvim/lua/snippets/init.lua
@@ -32,19 +32,93 @@ local types = require("luasnip.util.types")
 local parse = require("luasnip.util.parser").parse_snippet
 
 
+local function count(_, _, old_state)
+    old_state = old_state or {
+        updates = 0
+    }
+
+    old_state.updates = old_state.updates + 1
+
+    local snip = sn(nil, { t(tostring(old_state.updates))})
+
+    snip.old_state = old_state
+    return snip
+end
+
 ls.add_snippets("all", {
-  s("ternary", {
-    i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
-  })
+    s("trig", {
+        i(1, "change to update"),
+        d(2, count, {1})
+    }),
+    s("parent_change", {
+        c(1, {
+            sn(nil, { t("("), r(1, "user_text"), t(")")}),
+            sn(nil, { t("["), r(1, "user_text"), t("]")}),
+            sn(nil, { t("{"), r(1, "user_text"), t("}")}),
+        }),
+    }, {
+        stored = {
+            ["user_text"] = i(1, "default_text")
+        }
+    })
+})
+
+
+ls.add_snippets("lua", {
+    s("fn", {
+        t("function "), i(1), t("("), i(2, "args"), t(")"),
+        t({ "", "\t" }), i(3, "process"), i(0),
+        t({ "", "end" })
+    }),
+})
+
+
+ls.add_snippets("markdown", {
+    s({ trig = "h(%d)", regTrig = true }, {
+        f(function(args, snip)
+            local ret = ""
+            for i = 1, snip.captures[1], 1 do
+                ret = ret .. "#"
+            end
+            ret = ret .. " "
+            return ret
+        end, {})
+    }),
+    s("->>", {
+        i(1, "send"), t(" ->> "), i(2, "recv"), t(" : "), i(0, "msg")
+    }),
+})
+
+
+ls.add_snippets("sh", {
+    s("#!", {
+        t("#!/"), i(1, "bin/bash"), i(0)
+    }),
+    s("#name", {
+        t("# NAME:   "),
+        d(1, function(args)
+            local percent_echo = vim.api.nvim_exec("echo @%", true)
+            return sn(nil, {
+                i(1, percent_echo)
+            })
+        end)
+    }),
+    s("#author", {
+        t("# AUTHOR: "), i(1, "marsh"), i(0)
+    }),
+    s("fn", {
+        i(1, "fun_name"), t("() {"),
+        t({ "", "\t" }), i(0),
+        t({ "", "}" }),
+    })
 })
 
 
 ls.add_snippets("vim", {
-  s("vimgrep", {
-    -- vimgrep! 
-    t("vimgrep "), t("/"), i(1, "pattern"), t("/j "), i(2, "%")
-  })
+    s("vimgrep", {
+        -- vimgrep!
+        t("vimgrep "), t("/"), i(1, "pattern"), t("/j "), i(2, "%")
+    })
 })
 
-
-
+-- vim: sw=4 sts=4 expandtab fenc=utf-8
