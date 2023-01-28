bc46ef4e8bf2e474889776ebab102b2f789427fc
marsh31 <marsh22h53@gmail.com>
2023-01-28 17:21:52 +0900

    fix: format

--- config/nvim/lua/snippets/init.lua
+++ config/nvim/lua/snippets/init.lua
@@ -4,7 +4,6 @@
 --
 --
 
-
 local ls = require("luasnip")
 local s = ls.snippet
 local sn = ls.snippet_node
@@ -31,15 +30,14 @@ local postfix = require("luasnip.extras.postfix").postfix
 local types = require("luasnip.util.types")
 local parse = require("luasnip.util.parser").parse_snippet
 
-
 local function count(_, _, old_state)
     old_state = old_state or {
-        updates = 0
+        updates = 0,
     }
 
     old_state.updates = old_state.updates + 1
 
-    local snip = sn(nil, { t(tostring(old_state.updates))})
+    local snip = sn(nil, { t(tostring(old_state.updates)) })
 
     snip.old_state = old_state
     return snip
@@ -47,31 +45,87 @@ end
 
 ls.add_snippets("all", {
     s("trig", {
-        i(1, "change to update"),
-        d(2, count, {1})
+        i(1, "change to update"), d(2, count, { 1 }),
     }),
-    s("parent_change", {
+
+    s("pnode", {
         c(1, {
-            sn(nil, { t("("), r(1, "user_text"), t(")")}),
-            sn(nil, { t("["), r(1, "user_text"), t("]")}),
-            sn(nil, { t("{"), r(1, "user_text"), t("}")}),
+            sn(nil, { t("("), r(1, "user_text"), t(")") }),
+            sn(nil, { t("["), r(1, "user_text"), t("]") }),
+            sn(nil, { t("{"), r(1, "user_text"), t("}") }),
         }),
     }, {
         stored = {
-            ["user_text"] = i(1, "default_text")
-        }
-    })
-})
-
-
-ls.add_snippets("lua", {
-    s("fn", {
-        t("function "), i(1), t("("), i(2, "args"), t(")"),
-        t({ "", "\t" }), i(3, "process"), i(0),
-        t({ "", "end" })
+            ["user_text"] = i(1, "default_text"),
+        },
     }),
 })
 
+ls.add_snippets("lua", {
+    s("fn", {
+        t("function "),
+        i(1),
+        t("("),
+        i(2, "args"),
+        t(")"),
+        t({ "", "\t" }),
+        i(3, "process"),
+        i(0),
+        t({ "", "end" }),
+    }),
+
+    s("dict", {
+        i(1, "key"),
+        t(" = "),
+        c(2, {
+            sn(nil, { i(1, "value") }),
+            sn(nil, {
+                t("{"),
+                t({ "", "\t" }),
+                i(1, "value"),
+                t({ "", "}" }),
+            }),
+        }),
+        t(","),
+        i(0),
+    }),
+
+    s("if", {
+        t("if "),
+        i(1, "condition"),
+        t(" then"),
+        t({ "", "\t" }),
+        i(2, "process"),
+        t({ "", "" }),
+        c(3, {
+            sn(nil, { t("elif"), i(1) }),
+            sn(nil, { t("else"), i(1) }),
+            sn(nil, { t(""), i(1) })
+        }),
+        t({ "", "end" }),
+        i(0),
+    }),
+
+    s("elif", {
+        t("elseif "), i(1, "condition"), t(" then"),
+        t({ "", "\t" }), i(2, "process"),
+        t({ "", "" }), c(3, {
+            sn(nil, { t("elif"), i(1) }),
+            sn(nil, { t("else"), i(1) }),
+            sn(nil, { t(""), i(1) }),
+        }),
+    }),
+
+    s("else", {
+        t("else"),
+        t({ "", "\t" }), i(2, "process"),
+        t({ "", "" }), c(3, {
+            sn(nil, { t("elif"), i(1) }),
+            sn(nil, { t("else"), i(1) }),
+            sn(nil, { t(""), i(1) }),
+        }),
+    }),
+})
 
 ls.add_snippets("markdown", {
     s({ trig = "h(%d)", regTrig = true }, {
@@ -82,43 +136,55 @@ ls.add_snippets("markdown", {
             end
             ret = ret .. " "
             return ret
-        end, {})
+        end, {}),
     }),
     s("->>", {
-        i(1, "send"), t(" ->> "), i(2, "recv"), t(" : "), i(0, "msg")
+        i(1, "send"),
+        t(" ->> "),
+        i(2, "recv"),
+        t(" : "),
+        i(0, "msg"),
     }),
 })
 
-
 ls.add_snippets("sh", {
     s("#!", {
-        t("#!/"), i(1, "bin/bash"), i(0)
+        t("#!/"),
+        i(1, "bin/bash"),
+        i(0),
     }),
     s("#name", {
         t("# NAME:   "),
         d(1, function(args)
             local percent_echo = vim.api.nvim_exec("echo @%", true)
             return sn(nil, {
-                i(1, percent_echo)
+                i(1, percent_echo),
             })
-        end)
+        end),
     }),
     s("#author", {
-        t("# AUTHOR: "), i(1, "marsh"), i(0)
+        t("# AUTHOR: "),
+        i(1, "marsh"),
+        i(0),
     }),
     s("fn", {
-        i(1, "fun_name"), t("() {"),
-        t({ "", "\t" }), i(0),
+        i(1, "fun_name"),
+        t("() {"),
+        t({ "", "\t" }),
+        i(0),
         t({ "", "}" }),
-    })
+    }),
 })
-
 
 ls.add_snippets("vim", {
     s("vimgrep", {
         -- vimgrep!
-        t("vimgrep "), t("/"), i(1, "pattern"), t("/j "), i(2, "%")
-    })
+        t("vimgrep "),
+        t("/"),
+        i(1, "pattern"),
+        t("/j "),
+        i(2, "%"),
+    }),
 })
 
 -- vim: sw=4 sts=4 expandtab fenc=utf-8
