1cb9f27f56eb82d417c2bbc100ec390ba7aaefaf
marsh31 <marsh22h53@gmail.com>
2023-05-06 17:03:55 +0900

    add: rust config for nvim

--- config/nvim/lua/snippets/rust.lua
+++ config/nvim/lua/snippets/rust.lua
@@ -31,7 +31,123 @@ local types = require("luasnip.util.types")
 local parse = require("luasnip.util.parser").parse_snippet
 
 local snippets = {
+    -- test
+    s("testmodule", {
+        t({ "#[cfg(test)]", "" }),
+        t({ "mod tests {", "" }),
+        t({ "\tuse super::*;", "", "" }),
+        t({ "\ttestfn" }),
+        i(0),
+        t({ "", "}" }),
+    }),
+    s("testfn", {
+        t({ "#[test]", "" }),
+        t({ "fn " }),
+        i(1, "testfunc_name"),
+        t({ "() {", "\t" }),
+        i(0),
+        t({ "", "}" }),
+    }),
+
+    -- fn
+    s("fn", {
+        c(1, {
+            sn(nil, { t("pub "), i(1) }),
+            sn(nil, { t(""), i(1) }),
+        }),
+        t({ "fn " }),
+        r(2, "func_name"),
+        t({ "(" }),
+        i(3, "arg"),
+        t({ ")" }),
+        c(4, {
+            sn(nil, { t(" -> "), i(1, "type") }),
+            sn(nil, { i(1, "") }),
+        }),
+        c(5, {
+            sn(nil, { t({ "", "where " }), i(1, "condition"), t({ "", "" }) }),
+            sn(nil, { t(" "), i(1) }),
+        }),
+        t({ "{", "\t" }),
+        r(6, "process"),
+        i(0),
+        t({ "", "}" }),
+    }, {
+        stored = {
+            ["func_name"] = i(1, "func_name"),
+            ["process"] = i(2, "unimplemented!();"),
+        },
+    }),
+
+    -- match
+    s("match", {
+        t("match "),
+        i(1, "value"),
+        t({ " {", "\t" }),
+        t("matchcase"),
+        i(2),
+        t({ "", "}" }),
+    }),
+
+    s("matchcase", {
+        c(1, {
+            sn(nil, {
+                i(1, "value"),
+                t(" => "),
+                r(2, "exp"),
+                t({ ",", "" }),
+            }),
+            sn(nil, {
+                t("_ => "),
+                r(1, "exp"),
+                t({ ",", "" }),
+            }),
+        }),
+        c(2, {
+            sn(nil, { t("matchcase"), i(1) }),
+            sn(nil, { t(""), i(1) }),
+        }),
+    }, {
+        stored = {
+            ["exp"] = i(1, "exp"),
+        },
+    }),
+
+    -- if
+    s("if", {
+        t({ "if " }),
+        i(1, "condition"),
+        t({ " {", "\t" }),
+        i(2, "proc"),
+        t({ "", "}" }),
+    }),
+
+    s("else", {
+        t({ "else {", "\t" }),
+        i(1, "proc"),
+        t({ "", "}" }),
+    }),
+
+    s("iflet", {
+        t({ "if let " }),
+        i(1, "value"),
+        t(" = "),
+        i(2, "some_value"),
+        t({ " {", "\t" }),
+        i(2, "proc"),
+        t({ "", "}" }),
+    }),
+
+    -- for
+    s("for", {
+        t({ "for " }),
+        i(1, "i"),
+        t(" in "),
+        i(2, "range"),
+        t({ " {", "\t" }),
+        i(3, "proc"),
+        t({ "", "}" }),
+    }),
 }
 
 return snippets
-
